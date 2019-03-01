b5373727a45d314bd0ea8e2a0fec713d0c53d4b9
michael <michael.medvedskiy.private@gmail.con>
2019-03-01 21:47:34 +0300

    first commit

---
+++ training/config_training.py
@@ -0,0 +1,17 @@
+config = {'stage1_data_path':'./../drive/My Drive/work/DataBowl3/stage1/stage1/',
+          'luna_raw':'./../drive/My Drive/work/DataBowl3/luna/raw/',
+          'luna_segment':'./../drive/My Drive/work/DataBowl3/luna/seg-lungs-LUNA16/',
+          
+          'luna_data':'./../drive/My Drive/work/DataBowl3/luna/allset',
+          'preprocess_result_path':'./../drive/My Drive/work/DataBowl3/stage1/preprocess/',
+          
+          'luna_abbr':'./detector/labels/shorter.csv',
+          'luna_label':'./detector/labels/lunaqualified.csv',
+          'stage1_annos_path':['./detector/labels/label_job5.csv',
+                './detector/labels/label_job4_2.csv',
+                './detector/labels/label_job4_1.csv',
+                './detector/labels/label_job0.csv',
+                './detector/labels/label_qualified.csv'],
+          'bbox_path':'../detector/results/res18/bbox/',
+          'preprocessing_backend':'python'
+         }
